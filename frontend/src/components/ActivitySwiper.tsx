a1c72577463e3a5cf2af1b6703c44a28ffaabc0c
AnaMilivojevic <anamilivojevic996@gmail.com>
2023-03-20 03:17:44 +0100

    updated card layout

--- frontend/src/components/ActivitySwiper.tsx
+++ frontend/src/components/ActivitySwiper.tsx
@@ -2,16 +2,17 @@ import ActivitySwiperItem from "./ActivitySwiperItem";
 import "./ActivitySwiper.scss";
 
 const ActivitySwiper = ({ activities }: ActivitiesProps): JSX.Element => {
+  // row overflow-auto flex-row flex-nowrap align-items-stretch gap-3
   return (
-    <div
-      id="activity-swiper"
-      className="row overflow-auto flex-row flex-nowrap align-items-stretch gap-3">
+    <div className="row align-items-stretch">
       {activities.map((el) => {
         return (
           <div
-            className="col-12 col-md-6 col-lg-4 px-0 card h-100 c-text-dark-green c-bg-beige"
-            key={el.id}>
-            <ActivitySwiperItem activity={el} />
+            key={el.id}
+            className="col-12 col-md-6 col-lg-4 mb-3 d-flex align-items-stretch">
+            <div className="card mb-3 c-text-dark-green c-bg-beige">
+              <ActivitySwiperItem activity={el} />
+            </div>
           </div>
         );
       })}
--- frontend/src/components/ActivitySwiperItem.tsx
+++ frontend/src/components/ActivitySwiperItem.tsx
@@ -1,28 +1,29 @@
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const ActivitySwiperItem = ({ activity }: ActivityProps): JSX.Element => {
   return (
     <>
-      <div className="card-img-wrapper">
+      <div className="card-img-wrapper mb-2">
         <img
           src={require(`../resources/images/${activity.imgFileName}`)}
           alt={`Image of ${activity.name}`}
           className="card-img-top"
         />
       </div>
-      <div className="card-body d-flex flex-column justify-content-between">
-        <div className="mb-2">
+      <div className="card-body py-3 d-flex flex-column justify-content-between">
+        <div className="mb-3">
           <h5 className="card-title mb-3 text-uppercase">{activity.name}</h5>
           <p className="card-text c-text-small">{activity.description}</p>
         </div>
         <div>
           <p className="card-text">
-            <a
+            <Link
               className="text-uppercase text-decoration-none c-text-wine fs-6 d-flex align-items-center"
-              href="#">
+              to="/search">
               Locations
               <MdKeyboardDoubleArrowRight className="ms-1" />
-            </a>
+            </Link>
           </p>
         </div>
       </div>
